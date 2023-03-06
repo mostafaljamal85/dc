690bc6ba468dfea4354936530f72b8c3d9b70b4c
mostafaljamal85 <mostafa.aljamal@st.ul.edu.lb>
2023-03-06 23:24:29 +0100

    mod1

--- script.js
+++ script.js
@@ -23,6 +23,7 @@ fetch('https://restcountries.com/v3.1/all')
                 <div class="card-body">
                   <p class="card-title"><span><b>Capital : </b></span> ttle</p>
                   <p class="card-title"><span><b>Population : </b></span> pop</p>
+                  <p class="card-title"><span><b>Area : </b></span> areas</p>
                   <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                   <a href="#" class="btn btn-primary">Go somewhere</a>
                 </div>
@@ -32,6 +33,7 @@ fetch('https://restcountries.com/v3.1/all')
               var boot =content.replace('...',element.flags.png)
               boot=boot.replace('ttle',element.capital)
               boot=boot.replace('pop',element.population)
+              boot=boot.replace('areas',element.area)
               _div.innerHTML=boot
               _karim.appendChild(_div)
             }
