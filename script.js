c30c331a9601755d45d1a183c2ba0c70a328911c
mostafaljamal85 <mostafa.aljamal@st.ul.edu.lb>
2023-03-09 01:04:32 +0100

    add borders and timezone

--- script.js
+++ script.js
@@ -17,17 +17,20 @@ fetch('https://restcountries.com/v3.1/all')
       .then(json => {
         json.forEach(element => {
             if(element.name.common==_selected){
+              console.log(element)
                 var content=`
                 <div class="card" style="width: 18rem;">
-                <img src="..." class="card-img-top" id="img1" alt="...">
+                <img src="image" class="card-img-top" id="img1" alt="image">
                 <div class="card-body" >
                   <p class="card-title"><span><b>Capital : </b></span> ttle</p>
                   <p class="card-title"><span><b>Population : </b></span> pop</p>
                   <p class="card-title"><span><b>Area : </b></span> areas</p>
                   <p class="card-title"><span><b>Continent : </b></span> cont</p>
                   <div class="card-title"><span><b>Currencies : </b></span>cur</div>
-
                   <p class="card-title"><span><b>Languages : </b></span>lan</p>
+                  <p class="card-title"><span><b>Timezone : </b></span>tz</p>
+                  <p class="card-title"><span><b>Borders : </b></span>..,..,..,..,..,..,..</p>
+                 
                   <button href="#" class="btn btn-dark" id="btn1">More details</button>
                 </div>
               </div>
@@ -36,20 +39,27 @@ fetch('https://restcountries.com/v3.1/all')
                   
                 
               let _div=document.createElement('div');
-              let boot =content.replace('...',element.flags.png)
+              let boot =content.replace('image',element.flags.png)
               boot=boot.replace('ttle',element.capital)
               boot=boot.replace('pop',element.population)
               boot=boot.replace('areas',element.area)
               boot=boot.replace('cont',element.continents)
-              // boot=boot.replace('lang',element.languages)
               var curr=element.currencies
-                  Object.keys(curr).forEach(currency=>{
-                     boot=boot.replace('cur',currency)
+                  Object.values(curr).forEach(currency=>{
+                     boot=boot.replace('cur',currency.name+'=> '+currency.symbol)
                   })
               var lang=element.languages
-                  Object.keys(lang).forEach(language=>{
+                  Object.values(lang).forEach(language=>{
                      boot=boot.replace('lan',language)
                   })
+              var bor=element.borders
+              if(bor!=null){
+                bor.forEach(border=>{
+                  boot=boot.replace('..',border)
+               })
+              }
+              boot=boot.replace('tz',element.timezones[0])
+
               _div.innerHTML=boot
               _karim.appendChild(_div)
 
